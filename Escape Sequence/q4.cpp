b640dc8e2f11fdc1d2aec7526ac71e8c32f96f88
Agastya Hukoo <agastyahukoo@gmail.com>
2024-05-02 16:36:07 +0530

    Escape Sequence

---
+++ Escape Sequence/q4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1\n1\t2\n1\t2\t3\n1\t2\t3\t4";
+    return 0;
+}
---
+++ Escape Sequence/q6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t\t\t4\n\t\t4\t3\n\t4\t3\t2\n4\t3\t2\t1\n";
+    return 0;
+}
