577d244c5939b702f2d356ab2e83498271ca978b
Agastya Hukoo <agastyahukoo@gmail.com>
2024-05-05 13:48:23 +0530

    Conditional Statements

--- Conditional Statements/q53.cpp
+++ Conditional Statements/q53.cpp
@@ -4,10 +4,14 @@ using namespace std;
 int main()
 {
     int a, b;
-    cout << "Enter two numbers.";
+    cout << "Enter two numbers.\n";
     cin >> a;
     cin >> b;
     if(a>b)
-    
+        cout << a << " is greater than " << b;
+    else if(b>a)
+        cout << b << " is greater than " << a;
+    else
+        cout << "Both numbers are equal";
     return 0;
 }
